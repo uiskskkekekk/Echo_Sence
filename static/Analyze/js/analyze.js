3ceb61db2ed9cfcc7fb7e4180822fe538e7acf4f
uiskskkekekk <angus.lu0611@gmail.com>
2025-01-03 01:00:22 +0800

    updata

--- static/Analyze/js/analyze.js
+++ static/Analyze/js/analyze.js
@@ -13,6 +13,31 @@ $.ajax({
     data: data,
     success: (res) => {
         console.log(res);
+
+        // 清空現有的相似音樂區域
+        const similarMusicContainer = document.querySelector('.similar-section .similar-music');
+
+        // 假設回應中有 "info" 陣列，我們遍歷它並生成 HTML
+        res.info.forEach((music) => {
+            // 創建音樂項目元素
+            const musicItem = document.createElement('div');
+            musicItem.classList.add('music-item');
+
+            // 設置內部HTML
+            musicItem.innerHTML = `
+                <div class="thumbnail-placeholder">
+                    <img src="${music.cover_url}" alt="${music.title}" />
+                </div>
+                <div class="music-info">
+                    <p><strong>${music.title}</strong></p>
+                    <p>${music.author}</p>
+                    <p>URL: <a href="${music.youtube_url}" target="_blank">${music.youtube_url}</a></p>
+                </div>
+            `;
+
+            // 將新創建的音樂項目加入到相似音樂容器中
+            similarMusicContainer.appendChild(musicItem);
+        });
     },
     error: (res) => {
         console.error("Error:", res);
