9c1986804df555556f613bd87dcc172c7ae9008a
uiskskkekekk <angus.lu0611@gmail.com>
2025-01-03 02:42:29 +0800

    update

--- static/Analyze/js/analyze.js
+++ static/Analyze/js/analyze.js
@@ -15,9 +15,29 @@ $.ajax({
         console.log(res);
 
         // 清空現有的相似音樂區域
+        const MusicContainer = document.querySelector('.music-section .your-music')
         const similarMusicContainer = document.querySelector('.similar-section .similar-music');
 
-        // 假設回應中有 "info" 陣列，我們遍歷它並生成 HTML
+        if (res.original_info) {
+            const originalMusic = res.original_info
+            const musicItem = document.createElement('div');
+            musicItem.classList.add('music-item');
+
+            // 設置內部HTML
+            musicItem.innerHTML = `
+                <div class="thumbnail-placeholder">
+                    <img src="${originalMusic.cover_url}" alt="${originalMusic.title}" />
+                </div>
+                <div class="music-info">
+                    <p><strong>${originalMusic.title}</strong></p>
+                    <p>${originalMusic.author}</p>
+                    <p>URL: <a href="${originalMusic.youtube_url}" target="_blank">${originalMusic.youtube_url}</a></p>
+                </div>
+            `;
+
+            MusicContainer.appendChild(musicItem);
+        }
+
         res.info.forEach((music) => {
             // 創建音樂項目元素
             const musicItem = document.createElement('div');
@@ -25,15 +45,15 @@ $.ajax({
 
             // 設置內部HTML
             musicItem.innerHTML = `
-                <div class="thumbnail-placeholder">
-                    <img src="${music.cover_url}" alt="${music.title}" />
-                </div>
-                <div class="music-info">
-                    <p><strong>${music.title}</strong></p>
-                    <p>${music.author}</p>
-                    <p>URL: <a href="${music.youtube_url}" target="_blank">${music.youtube_url}</a></p>
-                </div>
-            `;
+                        <div class="thumbnail-placeholder">
+                            <img src="${music.cover_url}" alt="${music.title}" />
+                        </div>
+                        <div class="music-info">
+                            <p><strong>${music.title}</strong></p>
+                            <p>${music.author}</p>
+                            <p>URL: <a href="${music.youtube_url}" target="_blank">${music.youtube_url}</a></p>
+                        </div>
+                    `;
 
             // 將新創建的音樂項目加入到相似音樂容器中
             similarMusicContainer.appendChild(musicItem);
