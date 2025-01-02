723e9c997931745cb20b00fcf1453c55bd13eb74
C111151132 <C111151132@nkust.edu.tw>
2025-01-03 01:56:28 +0800

    update

--- Music/models.py
+++ Music/models.py
@@ -22,16 +22,17 @@ class Artist(models.Model):
 
     @classmethod
     def get_artist(cls, artist_id: str) -> Manager["Artist"]:
-        return cls.objects.filter(artist_id=artist_id)
+        return cls.objects.filter(artist_id=artist_id).first()
 
 class Music(models.Model):
     music_id = models.CharField(primary_key=True, max_length=20)
     title = models.CharField(max_length=20, blank=True, null=True)
-    rating = models.IntegerField(default=0, null=True)
-    outer_url = models.URLField(max_length=500, blank=True, null=True)
-    rating_count = models.IntegerField(default=0)
-    listen_count = models.IntegerField(default=0)
+    youtube_url = models.URLField(max_length=500, blank=True, null=True)
+    cover_url = models.URLField(max_length=500, blank=True, null=True)
+    preview_url = models.URLField(max_length=1000, blank=True, null=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    view_count = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
     features = models.JSONField()
     
     class Meta:
@@ -39,25 +40,34 @@ class Music(models.Model):
         db_table = 'music'
 
     @classmethod
-    def upload_music(cls, music_id, info, features):
+    def upload_music(cls, info, features):
+        music_id = info.get('id')
         author_id = info.get('author_id')
         author = info.get('author')
         title = info.get('title')
         youtube_url = info.get('youtube_url')
+        cover_url = info.get('cover_url')
+        preview_url = info.get('preview_url')
+        view_count = info.get('view_count')
+        like_count = info.get('like_count')
 
-        artist = Artist.get_artist(author_id).first()
+        artist = Artist.get_artist(author_id)
         if artist is None: artist = Artist.create_artist(author_id, author)
             
         music = cls.objects.create(
             music_id = music_id,
             title = title,
-            outer_url = youtube_url,
+            youtube_url = youtube_url,
+            cover_url = cover_url,
+            preview_url = preview_url,
             artist = artist,
+            view_count = view_count,
+            like_count = like_count,
             features = features
         )
 
         return music.music_id
     
     @classmethod
-    def check_exists(cls, music_id):
-        return cls.objects.filter(music_id=music_id).exists()
+    def get_music_from_id(cls, music_id) -> "Music":
+        return cls.objects.filter(music_id=music_id).values().first()
--- Music/views.py
+++ Music/views.py
@@ -22,24 +22,26 @@ def upload_music(request: HttpRequest):
 
     if yt_link is None:
         return JsonResponse({"error": "The 'yt_link' field is missing."}, status=400)
-    
+
     data = {"yt_link": yt_link}
     
-    url = request.build_absolute_uri(reverse('full'))
+    url = request.build_absolute_uri(reverse('info'))
     response = requests.post(url, data=data)
-    resp_data = response.json()
-
-    features = resp_data.get('feature').get('data')
-    info = resp_data.get('info')
+    info = response.json()
     id = info.get('id')
 
-    if Music.check_exists(id): return JsonResponse({"data": id})
+    music = Music.get_music_from_id(id)
+    if music is not None: return JsonResponse({"data": music.music_id})
+
+    url = request.build_absolute_uri(reverse('feature'))
+    response = requests.post(url, data=data)
+    features = response.json().get('data')
 
     try:
-        res = Music.upload_music(music_id=id, info=info, features=features)
-        if res is None:
+        music_id = Music.upload_music(info=info, features=features)
+        if music_id is None:
             return JsonResponse({"error": "Music upload failed due to an unknown error."}, status=500)
-        return JsonResponse({"data": res})
+        return JsonResponse({"data": music_id})
     except Exception as e:
         error_id = uuid4()
         logger.error(f"{str(e)} ({error_id})")
@@ -52,30 +54,20 @@ def get_similiar_musics(request: HttpRequest):
 
     yt_link = request.POST.get("yt_link")
     data = {"yt_link": yt_link}
-    url = request.build_absolute_uri(reverse('full'))
-    response = requests.post(url, data=data)
-    resp_data = response.json()
     
-    info = resp_data.get('info')
-    id = info.get("id")
+    url = request.build_absolute_uri(reverse('info'))
+    response = requests.post(url, data=data)
+    info = response.json()
+    id = info.get('id')
 
-    print('id: ', id)
+    music = Music.get_music_from_id(id)
+    if music is None: return JsonResponse({"error": "Music has not been uploaded."}, status=500)
 
     try:
-        res = msc.compare(id)
-
-        new_info = []
-        for i in range(len(res)):
-            data = {"yt_link": res[i].get('outer_url')}
-            url = request.build_absolute_uri(reverse('info'))
-            response = requests.post(url, data=data)
-            resp_data = response.json()
-            new_info.append(resp_data)
-        
-
+        res = msc.compare(music.get('music_id'))
         if res is None:
             return JsonResponse({"error": "Music similarity comparison failed due to an unknown error."}, status=500)
-        return JsonResponse({"original_info": info, "info": new_info})
+        return JsonResponse({"original data": music, "data": res})
     except Exception as e:
         error_id = uuid4()
         logger.error(f"{str(e)} ({error_id})")
