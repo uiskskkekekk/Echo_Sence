f5d04219b9cf2ea2ba17e44a13b2b2e438877b9f
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2025-01-10 13:59:36 +0800

    update db

--- Echo_Sence/settings.py
+++ Echo_Sence/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_PATH = os.path.join(BASE_DIR, "static")
-
+APPEND_SLASH = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
